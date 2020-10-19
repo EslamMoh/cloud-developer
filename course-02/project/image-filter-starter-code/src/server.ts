0b7bad1d627d974aae603dd4996fc382823a40e4
Eslam Mohamed <eslammoh.ce@gmail.com>
2020-10-19 17:20:31 +0200

    add image url param validator

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -15,6 +15,11 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   app.get( "/filteredimage", async ( req, res ) => {
     let image_url = req.query.image_url
+
+    if (!image_url) {
+      return res.status(400).send({ message: 'Image Url is required or malformed' });
+    }
+
     let result = await filterImageFromURL(image_url)
     res.sendFile(result)
     setTimeout(function() {
